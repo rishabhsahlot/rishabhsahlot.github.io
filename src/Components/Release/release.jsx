952bb6a1bbbf1f28a67dd6e045af65cc36001d70
Rishabh Sahlot <rishabhsahlot.rs@gmail.com>
2021-06-29 20:19:46 -0400

    test completed and release starting

---
+++ src/Components/Release/release.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Container } from 'react-bootstrap';
+class ReleaseComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  <Container></Container>);
+    }
+}
+ 
+export default ReleaseComponent;
